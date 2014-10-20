968e411a6f3776e361e3f4828bc67feb238bebf4
mekagem <mekagem@gmail.com>
2014-10-20 16:47:06 +0400

    ls, cp , mv - yandex.disk restapi

---
+++ src/ru/yandex/restapi/DiskOperation.java
@@ -0,0 +1,107 @@
+package ru.yandex.restapi;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+
+enum DiskOperation {
+    LS("ls", 1, "GET") {
+        @Override
+        public String getURL(String resourcesURL, List<String> params) throws UnsupportedEncodingException {
+            String folderName = URLEncoder.encode(params.get(0), "UTF-8");
+            return resourcesURL + "?path=" + folderName;
+        }
+
+        @Override
+        public void processReply(String reply) {
+            JSONObject jsonObject = new JSONObject(reply);
+            if (jsonObject.has("_embedded")) {
+                // Directory
+                JSONArray files = jsonObject.getJSONObject("_embedded").getJSONArray("items");
+                for (int index = 0; index < files.length(); ++index) {
+                    JSONObject file = files.getJSONObject(index);
+                    System.out.println(file.getString("name"));
+                }
+            } else {
+                // File
+                System.out.println(jsonObject.getString("name"));
+            }
+        }
+    },
+    CP("cp", 2, "POST") {
+        @Override
+        public String getURL(String resourcesURL, List<String> params) throws UnsupportedEncodingException {
+            String from = URLEncoder.encode(params.get(0), "UTF-8");
+            String path = URLEncoder.encode(params.get(1), "UTF-8");
+            return resourcesURL + "/copy?from=" + from + "&path=" + path;
+        }
+
+        @Override
+        public void processReply(String reply) {
+            try {
+                JSONObject jsonObject = new JSONObject(reply);
+                System.out.println("OK");
+            } catch (JSONException e) {
+                System.out.println("ERROR");
+            }
+        }
+    },
+    MV("mv", 2, "POST") {
+        @Override
+        public String getURL(String resourcesURL, List<String> params) throws UnsupportedEncodingException {
+            String from = URLEncoder.encode(params.get(0), "UTF-8");
+            String path = URLEncoder.encode(params.get(1), "UTF-8");
+            return resourcesURL + "/move?from=" + from + "&path=" + path;
+        }
+
+        @Override
+        public void processReply(String reply) {
+            try {
+                JSONObject jsonObject = new JSONObject(reply);
+                System.out.println("OK");
+            } catch (JSONException e) {
+                System.out.println("ERROR");
+            }
+        }
+    };
+
+    private final String cmd;
+    private final int numberOfArguments;
+    private final String method;
+
+    DiskOperation(String cmd, int numberOfArguments, String method) {
+        this.cmd = cmd;
+        this.numberOfArguments = numberOfArguments;
+        this.method = method;
+    }
+
+    public String getCmd() {
+        return cmd;
+    }
+
+    public int getNumberOfArguments() {
+        return numberOfArguments;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public static DiskOperation getDiskOperation(String cmd) {
+        for (DiskOperation operation : DiskOperation.values()) {
+            if (operation.getCmd().equals(cmd)) {
+                return operation;
+            }
+        }
+        return null;
+    }
+
+    public abstract String getURL(String resourcesURL, List<String> params) throws UnsupportedEncodingException;
+
+    public abstract void processReply(String reply);
+}
--- src/ru/yandex/restapi/YandexDisk.java
+++ src/ru/yandex/restapi/YandexDisk.java
@@ -1,5 +1,6 @@
 package ru.yandex.restapi;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -12,68 +13,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class YandexDisk {
-    private static enum DiskOperation {
-        LS("ls", 1),
-        CP("cp", 2),
-        MV("mv", 2);
-
-        private final String cmd;
-        private final int numberOfArguments;
-
-        DiskOperation(String cmd, int numberOfArguments) {
-            this.cmd = cmd;
-            this.numberOfArguments = numberOfArguments;
-        }
-
-        public String getCmd() {
-            return cmd;
-        }
-
-        public int getNumberOfArgumnets() {
-            return numberOfArguments;
-        }
-
-        public static DiskOperation getDiskOperation(String cmd) {
-            try {
-                return DiskOperation.valueOf(cmd);
-            } catch (Exception e) {
-                return null;
-            }
-        }
-    }
-
-    private static String token = "df566bba13b547cc9dd37766554cc187";
     private static String resourcesURL = "https://cloud-api.yandex.net/v1/disk/resources";
 
     public static void main(String[] args) throws Exception {
-        DiskOperation operation = DiskOperation.getDiskOperation(args[0]);
+        final String token = args[0];
+        final DiskOperation operation = DiskOperation.getDiskOperation(args[1]);
         if (operation == null) {
-            throw new IllegalArgumentException("There is no command: " + args[0]);
+            throw new IllegalArgumentException("There is no command: " + args[1]);
         }
 
-//        List<String> params = new ArrayList<String>();
-//        for ()
+        final List<String> params = new ArrayList<String>();
+        if (args.length < 2 + operation.getNumberOfArguments()) {
+            throw new IllegalArgumentException("Command \"" + operation.getCmd() + "\" takes " + operation.getNumberOfArguments()
+                                                + " but only " + (args.length - 2) + " passed");
+        }
 
-        String folderName = URLEncoder.encode(args[0], "UTF-8");
-        URL requestURL = new URL(resourcesURL + "?path=" + folderName);
+        for (int index = 0; index < operation.getNumberOfArguments(); ++index) {
+            params.add(args[2 + index]);
+        }
+
+        String url = operation.getURL(resourcesURL, params);
+        URL requestURL = new URL(url);
         HttpURLConnection connection = (HttpURLConnection) requestURL.openConnection();
-        connection.setRequestMethod("GET");
+        connection.setRequestMethod(operation.getMethod());
         connection.setRequestProperty("Authorization", "OAuth " + token);
 
         int code = connection.getResponseCode();
-        InputStream inputStream = null;
-        if (200 <= code && code < 300) {
-            inputStream = connection.getInputStream();
-        } else if (400 <= code && code < 600) {
-            inputStream = connection.getErrorStream();
+        if (200 > code || code >= 300) {
+            throw new IllegalStateException("Connection error: code=" + code);
         }
 
-        if (inputStream != null) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        }
+        InputStream inputStream = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String reply = reader.readLine();
+        operation.processReply(reply);
     }
 }
